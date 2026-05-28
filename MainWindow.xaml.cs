b5af5ce68b460333dd25c83fd4a01326eb9db717
ST10492563 <ST10492563@rcconnect.edu.za>
2026-05-28 17:30:54 +0200

    Restore MainWindow.xaml.cs file content

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
 
-namespace CyberSecurtyChatBotPart2
+namespace CyberSecurtyChatBotPart2 
 {
     public partial class MainWindow : Window
     {
